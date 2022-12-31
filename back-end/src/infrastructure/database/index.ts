eea0f675b2ee63cdabaf66a2a62c40fa106d6df7
Felix developer <felixxdeveloper@gmail.com>
2022-12-31 17:23:59 -0300

    REF - Changing settings from application

--- back-end/src/infrastructure/database/index.ts
+++ back-end/src/infrastructure/database/index.ts
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
+import { mongoURL } from '../../secret';
 
 (async () => {
   try {
-    await mongoose.connect(process.env.URL_MONGO || '');
+    await mongoose.connect(process.env.URL_MONGO || mongoURL);
     console.log('Successful connection with DB')
   } catch (error) {
     return
