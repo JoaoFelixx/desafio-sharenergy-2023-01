ef069297b19a9cf2d51954684d68ecbea84136aa
Felix developer <felixxdeveloper@gmail.com>
2023-01-04 09:58:56 -0300

    ADD - Created global style

---
+++ front-end/src/styles/GlobalStyle.tsx
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  @font-face {
+
+  }
+
+  * {
+    padding: 0;
+    margin: 0;
+  }
+
+  body {
+   
+  }
+`;
