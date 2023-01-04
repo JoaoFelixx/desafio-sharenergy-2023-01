bd94d39fce0f374b75d3fc30abed9d3dc85276f4
Felix developer <felixxdeveloper@gmail.com>
2023-01-04 10:01:12 -0300

    ADD - Changed function name

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ApisProvider } from 'Context/ApisContext';
+import { AuthProvider } from 'Context/AuthContext';
+import { Routes } from 'Router';
 
-function App() {
+function Application() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <ApisProvider>
+        <Routes />
+      </ApisProvider>
+    </AuthProvider>
   );
 }
 
-export default App;
+export default Application;
