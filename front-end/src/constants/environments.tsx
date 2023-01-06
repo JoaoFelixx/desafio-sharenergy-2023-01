b4a1eab1c39786947f31ad36690120c76c247409
Felix developer <felixxdeveloper@gmail.com>
2023-01-06 10:21:27 -0300

    ADD - Adding statusCode to environments

--- front-end/src/constants/environments.tsx
+++ front-end/src/constants/environments.tsx
@@ -1,6 +1,8 @@
 import LocalizedStrings from "react-localization";
 import { urls } from './pt/urls';
 
+const statusCode = [100,101,102,200,201,202,204,303,305,307,400,401,402,403,404,405,408,409,500,501,502,503];
+
 export const environments = new LocalizedStrings({
-  pt: { ...urls }
+  pt: { ...urls, statusCode }
 });
