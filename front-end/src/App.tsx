6f839714b501fdb73612c3d3bb2b68cff3db191c
Felix developer <felixxdeveloper@gmail.com>
2023-01-06 10:26:15 -0300

    ADD - Adding toast and icon settings to project

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,12 +1,28 @@
-import { ApisProvider } from 'Context/ApisContext';
-import { AuthProvider } from 'Context/AuthContext';
+import "react-toastify/dist/ReactToastify.css";
 import { Routes } from 'Router';
+import { IconContext } from 'react-icons';
+import { ApisProvider } from 'Context/api-context';
+import { AuthProvider } from 'Context/auth-context';
+import { ToastContainer } from 'react-toastify';
+import { iconStyleSettings } from 'styles';
 
 function Application() {
   return (
     <AuthProvider>
       <ApisProvider>
-        <Routes />
+        <IconContext.Provider value={iconStyleSettings}>
+          <ToastContainer
+            position="top-right"
+            autoClose={4000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick={true}
+            rtl={false}
+            pauseOnFocusLoss={false}
+            draggable
+            pauseOnHover />
+          <Routes />
+        </IconContext.Provider>
       </ApisProvider>
     </AuthProvider>
   );
