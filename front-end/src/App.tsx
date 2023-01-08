87f3cbdcc7cda3784284fb616037abbe49f18b37
Felix developer <felixxdeveloper@gmail.com>
2023-01-08 09:54:43 -0300

    ADD - Added global style

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -5,24 +5,29 @@ import { ApisProvider } from 'Context/api-context';
 import { AuthProvider } from 'Context/auth-context';
 import { ToastContainer } from 'react-toastify';
 import { iconStyleSettings } from 'styles';
+import { ClientsContext } from 'Context/clients-context';
+import { GlobalStyle } from 'styles';
 
 function Application() {
   return (
     <AuthProvider>
       <ApisProvider>
-        <IconContext.Provider value={iconStyleSettings}>
-          <ToastContainer
-            position="top-right"
-            autoClose={4000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick={true}
-            rtl={false}
-            pauseOnFocusLoss={false}
-            draggable
-            pauseOnHover />
-          <Routes />
-        </IconContext.Provider>
+        <ClientsContext>
+          <IconContext.Provider value={iconStyleSettings}>
+            <ToastContainer
+              position="top-right"
+              autoClose={4000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick={true}
+              rtl={false}
+              pauseOnFocusLoss={false}
+              draggable
+              pauseOnHover />
+            <GlobalStyle />
+            <Routes />
+          </IconContext.Provider>
+        </ClientsContext>
       </ApisProvider>
     </AuthProvider>
   );
