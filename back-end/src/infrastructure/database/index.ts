00a347fe83a9a64a2967ddd7664f4b36a4d52fc9
Felix developer <felixxdeveloper@gmail.com>
2023-01-10 09:25:56 -0300

    REF - Changed the code

--- back-end/src/infrastructure/database/index.ts
+++ back-end/src/infrastructure/database/index.ts
@@ -1,10 +1,9 @@
 import mongoose from "mongoose";
-import { mongoURL } from '../../secret';
 
 (async () => {
   try {
     mongoose.set('strictQuery', false);
-    await mongoose.connect(process.env.URL_MONGO || mongoURL);
+    await mongoose.connect(process.env.URL_MONGO || '');
   } catch (error) {
     return
   }
