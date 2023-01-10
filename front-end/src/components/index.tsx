23839f760e41ca1decf0f520bb0880847e84e0fe
Felix developer <felixxdeveloper@gmail.com>
2023-01-10 08:32:45 -0300

    ADD - Providing all components

--- front-end/src/components/index.tsx
+++ front-end/src/components/index.tsx
@@ -1,11 +1,16 @@
+import { Modal } from './modal';
 import { Navbar } from './navbar';
 import { CardLogin } from './cards';
 import { Jumbotron } from './Jumbotron';
-import { LoginForm } from './forms';
 import { BackButton } from './buttons';
 import { UserPagination } from './paginations';
 import { UsersList, StatusList, ClientsList } from './lists';
-import { Modal } from './modal';
+import { 
+	LoginForm, 
+	ClientAddForm, 
+	ClientEditForm,
+	ClientRemoveForm, 
+} from './forms';
 
 export {
 	Modal,
@@ -17,5 +22,8 @@ export {
 	StatusList,
 	BackButton,
 	ClientsList,
+	ClientAddForm,
 	UserPagination,
+	ClientEditForm,
+	ClientRemoveForm,
 }
