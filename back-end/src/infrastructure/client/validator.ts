e37d0908808c01ffc2c50aadd6c086bc4113d367
Felix developer <felixxdeveloper@gmail.com>
2023-01-10 08:33:43 -0300

    REF - Adding validation on validator method

--- back-end/src/infrastructure/client/validator.ts
+++ back-end/src/infrastructure/client/validator.ts
@@ -1,7 +1,17 @@
+import { Client } from '../../domain/entities';
+
 interface ErrorValidator {
   error?: string;
 }
 
-export const validator = (): ErrorValidator => {
-  return {}
-} 
+export const validator = (client: Client): ErrorValidator => {
+  const keys = Object.keys(client);
+
+  const invalidProperties = keys
+    .filter((key) => !client[key as keyof Client])
+    .map((key) => `${key} is invalid`);
+
+  return {
+    error: invalidProperties.join(', ') || undefined
+  }
+}
